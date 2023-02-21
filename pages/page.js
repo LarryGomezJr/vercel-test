1d39f48c961d106bb06a4263ea82d46acdbd0bb6
Mr. Larry <larrygomez689@gmail.com>
2023-02-21 16:46:48 +0800

    create page in pages

---
+++ pages/page.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Navbar from "./navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold">Welcome to my page</h1>
+      </div>
+    </>
+  );
+}
